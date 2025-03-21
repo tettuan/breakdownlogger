bc20026fd267573ceb5d2d726e75b24034e56c4a
tettuan <tettuan@gmail.com>
2025-03-21 21:59:15 +0900

    docs: enhance module documentation for all entrypoints

--- mod.ts
+++ mod.ts
@@ -1,3 +1,20 @@
+/**
+ * @module breakdownlogger
+ * 
+ * A debug logging library for Deno applications, designed for use in test environments.
+ * This module provides a simple and flexible logging system that supports different log levels
+ * and can be configured through environment variables or programmatically.
+ * 
+ * @example
+ * ```ts
+ * import { BreakdownLogger, LogLevel } from "@tettuan/breakdownlogger";
+ * 
+ * const logger = new BreakdownLogger();
+ * logger.info("Application started");
+ * logger.debug("Debug information", { someData: "value" });
+ * ```
+ */
+
 export { BreakdownLogger } from "./src/logger.ts";
 export { LogLevel } from "./src/types.ts";
 export type { LogEntry, LoggerConfig } from "./src/types.ts";
